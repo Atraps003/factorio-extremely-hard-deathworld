6d229e5918f5ac24aa079a4133075775e8802a3a
CrypticError <contact-github@mailto.random.ink>
2024-06-13 16:48:27 -0700

    autoindent control.lua

--- control.lua
+++ control.lua
@@ -5,6 +5,6 @@ handler.add_lib(require("silo-script"))
 commands.add_command("reset", "Reset map", function(command)
 	local player = game.get_player(command.player_index)
 	if player.admin == true then
-	reset(string.format("%s has manually forced a reset.", player.name))
+		reset(string.format("%s has manually forced a reset.", player.name))
 	end
 end)
