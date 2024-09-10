f9c4b5036335bc26af05ca4e15d0a00c795465c7
Atraps003 <atraps003@gmail.com>
2024-09-10 00:49:12 -0500

    reset allows set seed

--- control.lua
+++ control.lua
@@ -2,9 +2,19 @@ local handler = require("event_handler")
 handler.add_lib(require("freeplay"))
 handler.add_lib(require("silo-script"))
 
-commands.add_command("reset", "Reset map", function(command)
+commands.add_command("reset", "Resets map with random seed. Accepts valid seed as parameter.", function(command)
 	local player = game.get_player(command.player_index)
 	if player.admin == true then
+		if command.parameter ~= nil then
+			if tonumber(command.parameter, 10) ~= nil then
+				local reset_seed = tonumber(command.parameter)
+				if reset_seed > 0 and reset_seed < 4294967296 then
+					global.reset_seed = reset_seed
+				end
+			end
+		else
+			global.reset_seed = math.random(1111, 4294967295)
+		end
 		reset(string.format("%s has manually forced a reset.", player.name))
 	end
 end)
